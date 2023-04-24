5c4204be0010d13a45253b3429eb541439756ce9
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-04-23 23:40:37 -0500

    updated maincontainer.js with import of pages

---
+++ my-portfolio/src/components/MainContainer.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import AboutME from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutME />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <>
+    <div className="App">
+      <div className="header">
+        <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      </div>
+      {renderPage()}
+    </div>
+    <Footer />
+    </>
+  );
+};
