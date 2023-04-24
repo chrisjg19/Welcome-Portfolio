443afefb79c4330501ab29d4ce033350c0e8b2b6
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-04-24 00:00:52 -0500

    did aboutme page

---
+++ my-portfolio/src/components/pages/AboutMe.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function AboutME() {
+    return (
+        <div className="page-container">
+            <h2 className="hello">Hello Im Chris, Welcome to my World!</h2>
+            <p>I am a early junior web develoer from Houston,TX. It can get pretty hot here, which is the perfect excuse for me to stay home and work on my coding skills.</p>
+            <p>Coding is new to me but I have always been one to push myself to learn something and to get good at it, as well as I have always had an interest on how the web and tech works.</p>
+            <p>When I am not coding you can usually find me in my room watching Youtube or a Futbol game. I also enjoy sleeping and listening to music. I try to also attend as many Mls or Houston Astros Games as possible.</p>
+            <p>Take it easy!</p>
+
+
+        </div>
+    )
+}
