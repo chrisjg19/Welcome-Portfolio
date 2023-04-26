0f1999356d0e10533626c3b459f3213aa199bda0
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-04-26 00:56:36 -0500

    update

--- my-portfolio/src/components/MainContainer.js
+++ my-portfolio/src/components/MainContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import Navbar from './Navbar';
 //import Header from '../components/Header';
 import Footer from '../components/Footer';
 import About from '../pages/About';
