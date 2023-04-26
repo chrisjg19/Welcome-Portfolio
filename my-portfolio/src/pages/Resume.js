e0feb64a1c7cb043ee4e4fb165d2869a12527064
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-04-26 00:47:21 -0500

    finished resume somewhat, still has issues

---
+++ my-portfolio/src/pages/Resume.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <section>
+    <div className='page-container'>
+    <a href="https://docs.google.com/document/d/0B6D304sYnwwgLXJweDFuaXpLVmRLQkptRlN1VkEtdERiN2ZN/edit?resourcekey=0-VAX_efnwsFxT14EsGfkgXg" >Download Resume</a>
+    <h2>Skills</h2>
+    <ul className='skills'>
+      <li>HTML5</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>Node.js</li>
+      <li>Express.js</li>
+      <li>React</li>
+      <li>Bootstrap</li>
+      <li>MongoDB</li>
+    </ul>
+    </div>
+    </section>
+  );
+};
+
+export default Resume;
