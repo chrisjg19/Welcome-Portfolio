c79b7824c081e23a1921420c4997f97d099f234e
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-04-30 22:51:19 -0500

    updated portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,41 @@
 import React from "react";
+import Proj001 from '../imgs/MasterMax5000.gif'
+
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const projects = [
+    {
+      id: 1,
+      title: "Quiz Master Max",
+      image: Proj001,
+      description: "A quiz app that allows users to take quizzes on a variety of programming topics.",
+      link: 'https://quizmax.herokuapp.com/login',
+      github: 'https://github.com/chrisjg19/QuizMasterMax5000'
+    },
+    {
+        id: 2,
+        title: "Quiz Master Max",
+        image: Proj001,
+        description: "A quiz app that allows users to take quizzes on a variety of programming topics.",
+        link: 'https://quizmax.herokuapp.com/login',
+        github: 'https://github.com/chrisjg19/QuizMasterMax5000'
+      },
+   
+  ];
+
+  return (
+    <div className="project-container">
+      {projects.map((project) => (
+        <div key={project.id} className="project-card">
+          <h3>{project.title}</h3>
+          <img src={project.image} alt={project.title} />
+          <p>{project.description}</p>
+          <a href={project.link}>Visit Website</a>
+          <a href={project.github}>View Code on GitHub</a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Portfolio;
