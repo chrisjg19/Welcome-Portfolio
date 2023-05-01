6c99b4dbb2a8e435e75637d0fe267e44afb1d6ae
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-04-30 22:54:11 -0500

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,18 @@
 import React from 'react';
-
-
+import { Container } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {AiFillLinkedin} from 'react-icons/ai'
 const Footer = () => {
   return (
     <footer>
-      <div className="footer">
-        <div className='footer-container'>
-        <a href="https://www.linkedin.com/in/christopher-gonzalez-73b927122/" target="_blank" rel="noreferrer" className='nav-link'>/LinkedIn </a>
-        <a href="https://github.com/chrisjg19" target="_blank" rel="noreferrer" className='nav-link'>/GitHub </a>
-        <a href="https://twitter.com/" target="_blank" rel="noreferrer" className='nav-link'>/Twitter</a>
-        </div>
-      </div>
+      <Navbar fixed='bottom'>
+        <Container>
+        <Nav.Link href="https://www.linkedin.com/in/christopher-gonzalez-73b927122/" target="_blank" rel="noreferrer" className='nav-link'> <AiFillLinkedin/> </Nav.Link>
+        <Nav.Link href="https://github.com/chrisjg19" target="_blank" rel="noreferrer" className='nav-link'>GitHub </Nav.Link>
+        <Nav.Link href="https://twitter.com/" target="_blank" rel="noreferrer" className='nav-link'>Twitter</Nav.Link>
+        </Container>
+      </Navbar>
     </footer>
   )
 };
